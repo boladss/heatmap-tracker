45f98ad02fccce5ecd37d667217fcc5c2f46a053
boladss <anton.boladss@gmail.com>
2024-04-06 14:36:11 +0800

    feat/style: add color range implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,59 @@
 import { useState } from 'react'
 import './App.css'
 
-function Day({ index }) {
+interface DayBox {
+  index: number;
+  value: number;
+  max: number;
+}
+
+function Day({ index, value, max }: DayBox) {
+  // hsl(0, 100%, 1.57%)
+  const minOpacity = 0.2;
+  const maxOpacity = 1;
+
+  const opacity = (((value/max) * (maxOpacity - minOpacity)) + minOpacity);
+  const boxColor = opacity ? { backgroundColor: `hsl(120, 100%, 25.1%, ${opacity}` }
+    : { backgroundColor: `hsl(0, 0%, 90%)` };
+
   return (
-    // <div className="box">{index}</div>
-    <div className="box"></div>
+    <div className="box" style={boxColor} />
   )
 }
 
 function HeatMap() {
-  const [timeline, setTimeline] = useState(Array(365).fill(0).map((_, i) => <Day index={i} />));
+  // Temporary Values
+  const values: number[] = Array(365).fill(0).map(() => Math.floor(Math.random() * 10));
+  const max: number = Math.max(...values);
+
+  const [timeline, setTimeline] = useState(Array(365).fill(0).map((_, i) =>
+    <Day 
+      index={i} 
+      value={values[i]} 
+      max={max}
+    />
+  ));
 
   return (
-    <div className="heatmap-container">
-      {timeline}
+    <div className="heatmap-wrapper">
+      <div className="heatmap-container">
+        {timeline}
+      </div>
     </div>
   )
 }
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div class="heatmap-wrapper">
+    <>
       <HeatMap />
-    </div>
+      <HeatMap />
+      <HeatMap />
+    </>
   )
 }
 
