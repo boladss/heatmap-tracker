72a66d4eec960f0574b2950372dada6b17533e3b
boladss <anton.boladss@gmail.com>
2024-04-06 12:10:47 +0800

    feat: static heatmap display

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 
+function Day({ index }) {
+  return (
+    // <div className="box">{index}</div>
+    <div className="box"></div>
+  )
+}
+
+function HeatMap() {
+  const [timeline, setTimeline] = useState(Array(365).fill(0).map((_, i) => <Day index={i} />));
+
+  return (
+    <div className="heatmap-container">
+      {timeline}
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div class="heatmap-wrapper">
+      <HeatMap />
+    </div>
   )
 }
 
