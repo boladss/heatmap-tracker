68f5e24a31f3cda55c80affdd4dd1a1e51866d5e
boladss <anton.boladss@gmail.com>
2024-04-17 12:41:32 +0800

    feat: add border highlighting for selected index

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-function Day({ key, input, max }: { key: number, input: number, max: number} ) {
+function Day({ index, input, max, selected }: { index: number, input: number, max: number, selected: number} ) {
   // const [value, setValue] = useState(0);
   
   // hsl(0, 100%, 1.57%)
@@ -12,19 +12,23 @@ function Day({ key, input, max }: { key: number, input: number, max: number} ) {
   const boxColor = opacity ? { backgroundColor: `hsl(120, 100%, 25.1%, ${opacity}` }
     : { backgroundColor: `hsl(0, 0%, 90%)` };
 
+  const borderColor = (index === selected) ? { borderColor: `hsla(0, 0%, 0%, 50%)` }
+    : { borderColor: `hsla(0, 0%, 0%, 10%)` };
+
   return (
-    <div className="box" style={boxColor}/>
+    <div className="box" style={{...boxColor, ...borderColor}} />
   )
 }
 
-function Timeline({ values }) {
+function Timeline({ values, selected }: { values: number[], selected: number}) {
   const max: number = Math.max(...values);
 
   const timeline = Array(365).fill(0).map((_, i) =>
     <Day 
-      key={i} 
+      index={i} 
       input={values[i]} 
       max={max}
+      selected={selected}
     />
   );
 
@@ -35,20 +39,18 @@ function Timeline({ values }) {
   )
 }
 
-function EditForm({ values, setValue }) {
-  const [index, setIndex] = useState(0);
-
-  function handleIncrement(index: number) {
+function EditForm({ values, setValue, selected, selectIndex }) {
+  function handleIncrement(selected: number) {
     const nextValues = values.map((c: number, i: number) => {
-      if (i === index) return c + 1;
+      if (i === selected) return c + 1;
       else return c;
     })
     setValue(nextValues);
   }
 
-  function handleDecrement(index: number) {
+  function handleDecrement(selected: number) {
     const nextValues = values.map((c: number, i: number) => {
-      if (i === index && c > 0) return c - 1;
+      if (i === selected && c > 0) return c - 1;
       else return c;
     })
     setValue(nextValues);
@@ -68,19 +70,19 @@ function EditForm({ values, setValue }) {
     <>
       <input
         type="number" 
-        value={index} 
-        onChange={e => setIndex(Number(e.target.value))}
+        value={selected} 
+        onChange={e => selectIndex(Number(e.target.value))}
         placeholder="Input index here."
       />
       <button
         className="value-buttons"
-        onClick={() => handleIncrement(index)}
+        onClick={() => handleIncrement(selected)}
       >
         INC
       </button>
       <button
         className="value-buttons"
-        onClick={() => handleDecrement(index)}
+        onClick={() => handleDecrement(selected)}
       >
         DEC
       </button>
@@ -101,18 +103,21 @@ function EditForm({ values, setValue }) {
 }
 
 function HeatMap() {
-  // Temporary Values
-  
-  // const [values, setValue] = useState(Array(365).fill(0).map(() => Math.floor(Math.random() * 10)));
   const [values, setValue] = useState(Array(365).fill(0));
+  const [selected, selectIndex] = useState(0); // selected index for highlighting
 
   return (
     <div className="heatmap-wrapper">
       <div className="heatmap-container">
-        <Timeline values={values}/>
+        <Timeline values={values} selected={selected}/>
       </div>
       <p>
-        <EditForm values={values} setValue={setValue}/>
+        <EditForm
+          values={values}
+          setValue={setValue}
+          selected={selected}
+          selectIndex={selectIndex}
+        />
       </p>
     </div>
   )
